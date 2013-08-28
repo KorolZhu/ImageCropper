235816a5f8a92c6fca6339a881a2aa08ed93ae50
zhu zhi <zhuzhi2013@gmail.com>
2013-08-28 17:46:21 +0800

    测试

--- ImageCroper/HTAppDelegate.m
+++ ImageCroper/HTAppDelegate.m
@@ -15,7 +15,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+     // Override point for customization after application launch.
     self.viewController = [[HTImageCroperViewController alloc] init];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
